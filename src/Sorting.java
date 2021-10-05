03a3241fe37cc13acf2e8cec315194b843dd5f79
wanna-be-da <ceh9856@snu.ac.kr>
2021-10-05 19:07:26 +0900

    add merge_sort, quick_sort

--- src/Sorting.java
+++ src/Sorting.java
@@ -4,9 +4,12 @@ public class Sorting {
     public static void main(String[] args){
         int[] arr = {1,5,6,24,2,5,1,5,5};
         //int[] sorted_arr1 = selection_sort(arr, arr.length);
-        int[] sorted_arr = insertion_sort(arr, arr.length);
+        //int[] sorted_arr = insertion_sort(arr, arr.length);
         //int[] sorted_arr = bubble_sort(arr, arr.length);
-        System.out.println(Arrays.toString(sorted_arr));
+        //int[] sorted_arr = shell_sort(arr, arr.length);
+        //merge_sort(arr,0, arr.length-1);
+        quick_sort(arr, 0, arr.length-1);
+        System.out.println(Arrays.toString(arr));
     }
 
     public static int[] selection_sort(int[] A, int n){
@@ -56,4 +59,97 @@ public class Sorting {
         A = bubble_sort(A, n-1);
         return A;
     }
+
+    public static int[] shell_sort(int[] A, int n){
+        //upgraded version of insertion_sort
+        int[] step ={1,3,7};
+        for (int i = step.length-1; i>= 0 ; i--){
+            step_insertion_sort(A, n, step[i]);
+        }
+        return A;
+    }
+
+    public static void step_insertion_sort(int[] A, int n, int step){
+        for (int i = 0; i<step; i++){
+            for (int j = i + step; j < n; j=j+step){
+                //insert A[j] in right place
+                for (int k = i; k<=j;k=k+step){
+                    if (A[j]<A[k]){
+                        //right shift and put it
+                        int temp = A[j];
+                        for (int m = j-step; m>=k; m = m-step){
+                            A[m+step] = A[m];
+                        }
+                        A[k] = temp;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    public static void merge_sort(int[] A, int p, int r){
+        if (r > p) {
+            int q = (r + p) / 2;
+            merge_sort(A, p, q);
+            merge_sort(A, q+1, r);
+            merge(A, p, q, r);
+        }
+    }
+
+    public static void merge(int[] A, int p, int q, int r){
+        // A[p...q] merge A[q+1,...,r]
+        int[] B = new int[r-p+1];
+        //set 3 pointers
+        int idx1 = p;
+        int idx2 = q+1;
+        int idx3 = 0;
+        while (idx1 < q+1 && idx2 < r+1){
+            if (A[idx1]<A[idx2]){
+                B[idx3++] = A[idx1++];
+            } else {
+                B[idx3++] = A[idx2++];
+            }
+        }
+
+        if (idx1 == q+1) {
+            while (idx2 < r+1){
+                B[idx3++] = A[idx2++];
+            }
+        } else if (idx2 == r+1){
+            while (idx1 < q+1){
+                B[idx3++] = A[idx1++];
+            }
+        }
+        while (idx3 >0){
+            A[r] = B[--idx3];
+            r--;
+        }
+    }
+
+    public static void quick_sort(int[] A, int p, int r){
+        if (p<r) {
+            int q = partition(A, p, r);
+            quick_sort(A, p, q - 1);
+            quick_sort(A, q + 1, r);
+        }
+    }
+
+    public static int partition(int A[], int p, int r){
+        int pivot = A[r];
+        int i = p-1; //end of zone1
+        for (int j=p;j<=r-1;j++) {
+            if (A[j] < pivot) {
+                int temp = A[++i];
+                A[i] = A[j];
+                A[j] = temp;
+            }
+        }
+        A[r] = A[i+1];
+        A[i+1] = pivot;
+
+        return i+1;
+    }
+
+    public static void heap_sort(int[] A)
 }
