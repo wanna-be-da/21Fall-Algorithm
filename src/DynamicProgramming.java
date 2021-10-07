c4495f5d718c8ceb25c548e4f00b9669bade0d61
wanna-be-da <ceh9856@snu.ac.kr>
2021-10-08 00:06:48 +0900

    fixed dp algo2

--- src/DynamicProgramming.java
+++ src/DynamicProgramming.java
@@ -4,7 +4,7 @@ public class DynamicProgramming {
     public static void main(String[] args){
         int[][] matrix = {{6,7,12,5},{5,3,11,18},{7,17,3,3},{8,10,14,9}};
         int[][] weight = {{6,7,12,-5,5,3,11,3},{-8,10,14,9,7,13,8,5},{11,12,7,4,8,-2,9,4}};
-        System.out.println(pebbleSum(weight, 4));
+        System.out.println(pebbleSum(weight, 2));
     }
 
     public static int matrixPath(int[][] matrix, int i, int j){
@@ -19,28 +19,28 @@ public class DynamicProgramming {
         }
         for (int m=1;m<=row;m++){
             for (int n=1;n<=col;n++){
-                c[m][n] = matrix[m-1][n-1] + max(c[m-1][n], c[m][n-1]);
+                c[m][n] = matrix[m-1][n-1] + max2(c[m-1][n], c[m][n-1]);
             }
         }
         return c[i][j];
     }
 
     private static int pebble(int[][] w, int i, int p){
-        int[][] c = new int[w[0].length][4];
+        int[][] c = new int[w[0].length + 1][4];
         //put c[0][0] ... c[0][3], pattern 0,1,2,3
         c[0][0] = w[0][0];
-        c[0][1] = w[0][1];
-        c[0][2] = w[0][2];
-        c[0][3] = w[0][0] + w[0][3];
+        c[0][1] = w[1][0];
+        c[0][2] = w[2][0];
+        c[0][3] = w[0][0] + w[2][0];
 
         //put things
         for (int m = 1; m<=w.length; m++){
             int [] prev_arr = {c[m-1][0], c[m-1][1], c[m-1][2], c[m-1][3]};
             int prev_max = max(prev_arr);
-            c[m][0] = w[m][0] + prev_max;
-            c[m][1] = w[m][1] + prev_max;
-            c[m][2] = w[m][2] + prev_max;
-            c[m][3] = w[m][0] + w[m][3] + prev_max;
+            c[m][0] = w[0][m] + prev_max;
+            c[m][1] = w[1][m] + prev_max;
+            c[m][2] = w[2][m] + prev_max;
+            c[m][3] = w[0][m] + w[2][m] + prev_max;
         }
 
         return c[i-1][p-1];
@@ -59,6 +59,14 @@ public class DynamicProgramming {
             }
         }
         return result;
+    }
+
+    private static int max2(int i, int j) {
+        if (i > j) {
+            return i;
+        } else {
+            return j;
+        }
     }
 
 
